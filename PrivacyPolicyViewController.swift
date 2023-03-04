982464f5d285d8aa9a7ce54edc878e79159f7328
mirsaitsabirzanov <mirsait77777@gmail.com>
2023-03-04 14:14:41 +0300

    changed the WebView

--- PrivacyPolicyViewController.swift
+++ PrivacyPolicyViewController.swift
@@ -9,19 +9,39 @@ import UIKit
 import WebKit
 
 class PrivacyPolicyViewController: UIViewController,  WKNavigationDelegate {
-
+    
+    @IBOutlet var navBarTitle: UINavigationItem!
+    
     @IBOutlet var webView: WKWebView!
+    
+    @IBAction func back(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let url = URL(string: "https://www.google.com")
         let request = URLRequest(url: url!)
-        
         webView.load(request)
-        webView.navigationDelegate = self
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGestureAction(_:)))
+        webView.addGestureRecognizer(panGesture)
+        
     }
     
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    
+    @objc func panGestureAction(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: webView)
+        let velocity = gesture.velocity(in: webView)
+        
+        if translation.x > 0 && velocity.x > 500 && webView.canGoBack {
+            webView.goBack()
         }
+        if translation.x < 0 && velocity.x < -500 && webView.canGoForward {
+                    webView.goForward()
+            }
     }
+}
